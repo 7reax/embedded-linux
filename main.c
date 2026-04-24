27b9da3dc6286077d7413b3ebb1782c080df9822
Julian <176830598+Angry007-Julian@users.noreply.github.com>
2026-04-24 15:14:25 +0200

    add w2

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+int main() {
+    printf("Hello World!\n");
+    printf("PID: %d\n", getpid());
+    printf("Programm läuft... (Ctrl+C zum Beenden)\n");
+
+    // Etwas Heap-Speicher allozieren damit /proc interessanter wird
+    char *heap_data = malloc(1024 * 1024); // 1 MB
+    if (heap_data) {
+        for (int i = 0; i < 1024 * 1024; i++) heap_data[i] = i % 256;
+    }
+
+    // Endlosschleife damit /proc analysiert werden kann
+    while (1) {
+        sleep(1);
+    }
+
+    free(heap_data);
     return 0;
 }
